2f78a2563bd00b3ec5b62aa0f755b6463a51bf8f
ChobbyCode <jacobhadfield99@gmail.com>
2024-01-29 17:40:40 +0000

    Added Themes

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # TerminalChadThemes
-A repository of all the themes available in base TerminalChad
+
+## Dependencies
+
+- [TerminalChad](https://github.com/ChobbyCode/TerminalChad/)
+
+## Installation
+
+Run the following command in command prompt:
+
+```
+terminalchad theme download chobbycode.terminalchadthemes / -m
+```
+
+### TerminalChad v0.1.2 or later
+
+```
+terminalchad theme reload
+```
---
+++ smooth-mint/startup-text.ps1
@@ -0,0 +1,16 @@
+Clear-Host;
+
+Write-Host "                                                           "
+Write-Host " _____ ___ ___ __ __ _ __  _  __  _      ____  _  __  __   "
+Write-Host "|_   _| __| _ \  V  | |  \| |/  \| |    / _/ || |/  \| _\  "
+Write-Host "  | | | _|| v / \_/ | | | ' | /\ | |_  | \_| >< | /\ | v | "
+Write-Host "  |_| |___|_|_\_| |_|_|_|\__|_||_|___|  \__/_||_|_||_|__/  "
+Write-Host "                                                           "
+Write-Host "      Windows Powershell (c) Microsoft Corporation         "
+Write-Host "   Terminal Chad (c) ChobbyCode 2024, All Rights Reserved  "
+Write-Host "                                                           "
+                                                                                                      
+                                                                                                      
+                                                                                                      
+                                                                                                      
+                                                                                                      
