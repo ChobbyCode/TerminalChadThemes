06c14f437eacc503861a937455134ccb16787d1c
ChobbyCode <jacobhadfield99@gmail.com>
2024-01-29 18:38:07 +0000

    Hacker Theme

---
+++ hacker/startup-text.ps1
@@ -0,0 +1,16 @@
+Clear-Host;
+
+Write-Host "                                                           " -f green
+Write-Host " _____ ___ ___ __ __ _ __  _  __  _      ____  _  __  __   " -f green
+Write-Host "|_   _| __| _ \  V  | |  \| |/  \| |    / _/ || |/  \| _\  " -f green
+Write-Host "  | | | _|| v / \_/ | | | ' | /\ | |_  | \_| >< | /\ | v | " -f green
+Write-Host "  |_| |___|_|_\_| |_|_|_|\__|_||_|___|  \__/_||_|_||_|__/  " -f green
+Write-Host "                                                           " -f green
+Write-Host "      Windows Powershell (c) Microsoft Corporation         " -f green
+Write-Host "   Terminal Chad (c) ChobbyCode 2024, All Rights Reserved  " -f green
+Write-Host "                                                           " -f green
+                                                                                                      
+                                                                                                      
+                                                                                                      
+                                                                                                      
+                                                                                                      
